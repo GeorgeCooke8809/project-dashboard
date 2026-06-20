76f5fffb5aa621d2455b423c56336a52854132a0
George Cooke <georgecooke8809@gmail.com>
2026-06-20 17:21:44 +0100

    made models.py

--- projects/models.py
+++ projects/models.py
@@ -3,3 +3,14 @@ from datetime import datetime
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from projects.db import Base
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id: Mapped[int] = mapped_column(primary_key=True, nullable=False)
+    name: Mapped[str] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False)
+    description: Mapped[str] = mapped_column(nullable=True)
+    datetime_created: Mapped[datetime] = mapped_column(nullable=False)
+    # ? favorite: Mapped[bool] = mapped_column(nullable=False)
+    # ? datetime_accessed: Mapped[datetime] = mapped_column(nullable=True)
--- run.py
+++ run.py
@@ -4,12 +4,12 @@ from dotenv import load_dotenv
 import logging
 logging.basicConfig(level=logging.DEBUG, filename="log.log", filemode="w", format="%(asctime)s - %(levelname)s - %(message)s")
 
-import projects
-
 load_dotenv("config.env")
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 DEBUGGING_STATE = bool(os.getenv("DEBUGGING"))
+
+import projects
 
 if __name__ == "__main__":
     print("hello world")
